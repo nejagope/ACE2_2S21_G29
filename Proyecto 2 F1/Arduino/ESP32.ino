dfbcd3d04b31f0f548a3befddf992f88ce0f977c
Abner-Hernandez <36278298+Abner-Hernandez@users.noreply.github.com>
2021-05-14 14:07:05 -0600

    Create ESP32.ino

---
+++ Proyecto 2 F1/Arduino/ESP32.ino
@@ -0,0 +1,30 @@
+#define RXD2 16 //desfinimos el pin 16 como serial de entrada
+#define TXD2 17 //desfinimos el pin 16 como serial de entrada
+
+#include "BluetoothSerial.h"
+BluetoothSerial SerialBT;
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)           //verifica que este activado el bt
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+void setup() {
+  Serial.begin(57600); //Serial por default
+  SerialBT.begin("Grupo29"); //Nombres del dispositivo BT
+  Serial.println("The device started, now you can pair it with bluetooth!");
+  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2); //iniciamos el nuevo puerto serial y setiamos parametros
+}
+
+void loop() 
+{
+  if(Serial2.available() > 0)
+  {
+    String lectura = Serial2.readString();
+    for(int i = 0 ; i < lectura.length(); i++)
+    {
+      SerialBT.write(lectura[i]);
+      Serial.write(lectura[i]);
+      delay(10);
+    }
+  }
+}
